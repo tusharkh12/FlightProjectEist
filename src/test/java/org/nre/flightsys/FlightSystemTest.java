e0c2b21f03df10e2109dd9b27563d937c7494f58
Shuhao Zhang <shuhao.zhang@tum.de>
2022-07-14 22:06:00 +0200

    fix raw use of generic List

---
+++ src/test/java/org/nre/flightsys/FlightSystemTest.java
@@ -0,0 +1,9 @@
+package org.nre.flightsys;
+
+public class FlightSystemTest {
+
+    @org.junit.jupiter.api.Test
+    public void test() {
+        System.out.println("noice");
+    }
+}
--- src/test/java/org/nre/flightsys/Test.java
+++
@@ -1,9 +0,0 @@
-package org.nre.flightsys;
-
-public class Test {
-
-    @org.junit.jupiter.api.Test
-    public boolean test() {
-        return true;
-    }
-}
