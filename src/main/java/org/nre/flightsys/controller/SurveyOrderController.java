32d6d7adf0911134ac6b463fa1a312c51560d5a4
Shuhao Zhang <shuhao.zhang@tum.de>
2022-07-14 22:30:33 +0200

    fix raw use of generic List

--- src/main/java/org/nre/flightsys/controller/SurveyOrderController.java
+++ src/main/java/org/nre/flightsys/controller/SurveyOrderController.java
@@ -2,8 +2,8 @@ package org.nre.flightsys.controller;
 
 import org.nre.flightsys.service.SurveyOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
