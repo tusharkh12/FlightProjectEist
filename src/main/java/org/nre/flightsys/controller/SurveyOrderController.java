d941de19c6809cbcd061786bed5ebf34c5f23503
ruanvillion <ruan.villion@gmail.com>
2022-07-18 15:39:04 +0200

    1

--- src/main/java/org/nre/flightsys/controller/SurveyOrderController.java
+++ src/main/java/org/nre/flightsys/controller/SurveyOrderController.java
@@ -3,6 +3,7 @@ package org.nre.flightsys.controller;
 import org.nre.flightsys.service.SurveyOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,25 +15,25 @@ public class SurveyOrderController {
     @Autowired
     SurveyOrderService SurveyOrderService;
 
-    @PostMapping("/order")
-    private ResponseEntity<String> takeOrder(@RequestParam("user") String user, @RequestParam("foodItem") String foodItem, @RequestParam("drinkItem") String drinkItem) {
+    @GetMapping("/order")
+    private String takeOrder(@RequestParam("user") String user, @RequestParam("foodItem") String foodItem, @RequestParam("drinkItem") String drinkItem) {
         try {
             SurveyOrderService.takeOrder(foodItem, drinkItem, user);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return ResponseEntity.ok(user);
+        return user;
         //return new String[]{foodItem, drinkItem, user};
     }
 
-    @PostMapping("/submit-survey")
-    private ResponseEntity<String> submitSurvey(@RequestParam("user") String user, @RequestParam("rating1") int rating1, @RequestParam("rating2") int rating2, @RequestParam("rating3") int rating3, @RequestParam("comment") String comment) {
+    @GetMapping("/submit-survey")
+    private String submitSurvey(@RequestParam("user") String user, @RequestParam("rating1") int rating1, @RequestParam("rating2") int rating2, @RequestParam("rating3") int rating3, @RequestParam("comment") String comment) {
         try {
             SurveyOrderService.submitSurvey(rating1, rating2, rating3, comment, user);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return ResponseEntity.ok(user);
+        return user;
         //return new String[]{Integer.toString(rating1), Integer.toString(rating2), Integer.toString(rating3), user};
     }
 
